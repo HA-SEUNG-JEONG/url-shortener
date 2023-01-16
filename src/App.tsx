ea8b065b686a68ba72d581f8ea459c32d224bc38
HA-SEUNG-JEONG <gktmd652@naver.com>
2023-01-16 22:38:35 +0900

    Feat : 전역에 react-toastify 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import InputBox from "./components/InputBox";
 import LinkResult from "./components/LinkResult";
@@ -9,6 +11,7 @@ function App() {
     <>
       <InputBox setInputValue={setInputValue} />
       <LinkResult inputValue={inputValue} />
+      <ToastContainer theme="colored" autoClose={3000} position="top-center" />
     </>
   );
 }
